456302669ba6cca7c00a18115ab412641094e013
TashfeenRao <raotashfeen25@gmail.com>
2020-04-18 01:55:22 +0500

    added sticler file

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ function Book(title, author, pages, readStatus = false) {
   this.pages = pages;
   this.readStatus = readStatus;
 }
+function displayBooks(book) {
+  const renderHook = document.getElementById('table');
+  renderHook.innerHTML = '';
+  book.forEach((b, index) => {
+    const tr = document.createElement('tr');
+    tr.className = 'table-info';
+    tr.innerHTML = `
+  <td>${b.title}</td>
+  <td>${b.author}</td>
+  <td>${b.pages}</td>
+  <td><button class="btn btn-warning " onclick="updatereadStatus(${index});">${
+  b.readStatus ? 'Unread' : 'Read'
+}</button></td>
+
+  <td><button class="btn btn-danger" onclick="removeBook(${index});">Remove</button></td>
+  `;
+    renderHook.append(tr);
+  });
+}
+
+function clearFields() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
+}
+
+function AddBook() {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const newBook = new Book(title, author, pages);
+  myLibrary.push(newBook);
+  displayBooks(myLibrary);
+  clearFields();
+}
 
 function updatereadStatus(index) {
   if (myLibrary[index].readStatus === true) {
@@ -16,48 +51,11 @@ function updatereadStatus(index) {
   displayBooks(myLibrary);
 }
 
-document
-  .getElementById("book-form")
-  .addEventListener("submit", function (event) {
+document.getElementById('book-form')
+  .addEventListener('submit', (event) => {
     event.preventDefault();
     AddBook();
   });
-
-function AddBook() {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const newBook = new Book(title, author, pages);
-  myLibrary.push(newBook);
-  displayBooks(myLibrary);
-  clearFields();
-}
-
-function clearFields() {
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("pages").value = "";
-}
-
-function displayBooks(book) {
-  const renderHook = document.getElementById("table");
-  renderHook.innerHTML = '';
-  book.forEach(function (b, index) {
-    const tr = document.createElement("tr");
-    tr.className = "table-info";
-    tr.innerHTML = `
-  <td>${b.title}</td>
-  <td>${b.author}</td>
-  <td>${b.pages}</td>
-  <td><button class="btn btn-warning " onclick="updatereadStatus(${index});">${
-      b.readStatus ? "Unread" : "Read"
-    }</button></td>
-
-  <td><button class="btn btn-danger" onclick="removeBook(${index});">Remove</button></td>
-  `;
-    renderHook.append(tr);
-  });
-}
 
 function removeBook(book) {
   delete myLibrary[book];
