d9a2998f0f4b0ac74120c7c73c2dbeb140f49dfd
TashfeenRao <raotashfeen25@gmail.com>
2020-04-17 22:33:41 +0500

    display books on table

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function Book(title, author, pages) {
   this.author = author;
   this.pages = pages;
 }
+Book.prototype.readStatus = function readStatus() {read = false}
+
+function changeStatus(book) {
+        
+
+}
 
 document
   .getElementById('book-form')
@@ -19,9 +25,26 @@ function AddBook() {
   const pages = document.getElementById('pages').value;
   const newBook = new Book(title, author, pages);
   myLibrary.push(newBook);
-  console.log(myLibrary);
+  myLibrary.forEach(function(book){
+    displayBooks(book);
+  })
+  clearFields();
 }
-
-function displayBooks() {}
-
-function changeStatus() {}
+function clearFields() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
+}
+function displayBooks(book) {
+  const renderHook = document.getElementById('table');
+  const tr = document.createElement('tr');
+  tr.className = 'table-info';
+  tr.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.pages}</td>
+  <td><button class="btn btn-warning">Read</button></td>
+  <td><button class="btn btn-danger">Remove</button></td>
+  `;
+  renderHook.append(tr);
+}
